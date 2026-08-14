17f59022de83e66c8f30db28db016cbceb697f09
Isaac Ogunmuko <opeeyemiisaac@gmail.com>
2026-08-14 14:59:14 -0400

    Resolve orderRoutes conflict

--- Backend/Routes/orderRoutes.js
+++ Backend/Routes/orderRoutes.js
@@ -5,8 +5,4 @@ const createOrder = require('./createOrder');
 
 router.post('/create', createOrder);
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 319a979a2a2e0b780aac1c22cf3cbeca96682525

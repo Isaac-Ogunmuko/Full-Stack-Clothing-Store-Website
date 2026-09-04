613967a67bdf05ada698c9c6409857411e59d7ce
Isaac-Ogunmuko <Isaac.Ogunmuko@gmail.com>
2026-09-03 23:44:04 -0400

    Fix file path casing for product model import

--- Backend/Routes/addProduct.js
+++ Backend/Routes/addProduct.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../Modals/Product'); 
+const Product = require('../Modals/product'); 
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path');
